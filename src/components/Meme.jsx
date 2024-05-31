70bf4ccb6cd066c8c9d6a69d056a9473b3a2c38d
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-05-31 13:23:11 +0200

    added accessibility input

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,8 +4,10 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <label> Top Text <br />
+        <input id="top-text" type="text" placeholder="Shut up" className="form--input" /></label>
+        <label> Bottom Text <br />
+        <input id="bottom-text" type="text" placeholder="and take my money" className="form--input" /></label>
         <button className="form--button">Get a new meme image 🖼️</button>
       </form>
     </main>
