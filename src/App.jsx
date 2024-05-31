eb4a7d2e42f32e2419b489e4a1d3768b76e7024f
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-05-31 11:02:05 +0200

    update to index.css, Index.html files and i created components folder so i can store my Meme generator components. And lastly i created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { } from 'react'
 import './App.css'
+import Header from '../src/components/Header'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
